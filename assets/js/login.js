633d5353ca2b4deba123c58e2ac1ea0a242ec6a6
Yoke <960567126@qq.com>
2020-10-06 15:45:16 +0800

    .

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,3 +1,12 @@
 $(function () {
-
+    // 点击去注册的链接
+    $('#link_reg').on('click', function () {
+        $('.login-box').hide();
+        $('.reg-box').show();
+    })
+    // 点击去登陆的链接
+    $('#link_login').on('click', function () {
+        $('.login-box').show();
+        $('.reg-box').hide();
+    })
 })
